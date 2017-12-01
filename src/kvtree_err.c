45fade12d4428cf6c7d0007c3884af457593689b
Gregory Becker <becker33@llnl.gov>
2017-12-01 13:07:07 -0800

    kvtree_err: changed warning messages SCR -> KVTree

--- src/kvtree_err.c
+++ src/kvtree_err.c
@@ -41,7 +41,7 @@ void kvtree_err(const char *fmt, ...)
   }
 
   va_list argp;
-  fprintf(stdout, "SCR %s ERROR: %s: ", KVTREE_VERSION, hostname);
+  fprintf(stdout, "KVTree %s ERROR: %s: ", KVTREE_VERSION, hostname);
   va_start(argp, fmt);
   vfprintf(stdout, fmt, argp);
   va_end(argp);
@@ -58,7 +58,7 @@ void kvtree_warn(const char *fmt, ...)
   }
 
   va_list argp;
-  fprintf(stdout, "SCR %s WARNING: %s: ", KVTREE_VERSION, hostname);
+  fprintf(stdout, "KVTree %s WARNING: %s: ", KVTREE_VERSION, hostname);
   va_start(argp, fmt);
   vfprintf(stdout, fmt, argp);
   va_end(argp);
@@ -78,7 +78,7 @@ void kvtree_dbg(int level, const char *fmt, ...)
   /*
   if (level == 0 || (kvtree_debug > 0 && kvtree_debug >= level)) {
   */
-    fprintf(stdout, "SCR %s: %s: ", KVTREE_VERSION, hostname);
+    fprintf(stdout, "KVTree %s: %s: ", KVTREE_VERSION, hostname);
     va_start(argp, fmt);
     vfprintf(stdout, fmt, argp);
     va_end(argp);
@@ -98,7 +98,7 @@ void kvtree_abort(int rc, const char *fmt, ...)
   }
 
   va_list argp;
-  fprintf(stderr, "SCR %s ABORT: %s: ", KVTREE_VERSION, hostname);
+  fprintf(stderr, "KVTree %s ABORT: %s: ", KVTREE_VERSION, hostname);
   va_start(argp, fmt);
   vfprintf(stderr, fmt, argp);
   va_end(argp);
