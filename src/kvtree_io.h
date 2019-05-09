facfa27ef4b6829ad986fad443d8ad5d18d96119
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2019-05-09 16:49:01 -0700

    gender in comments

--- src/kvtree_io.h
+++ src/kvtree_io.h
@@ -59,7 +59,7 @@ Basic File I/O
 =========================================
 */
 
-/* returns user's current mode as determine by his umask */
+/* returns user's current mode as determine by their umask */
 mode_t kvtree_getmode(int read, int write, int execute);
 
 /* open file with specified flags and mode, retry open a few times on failure */
