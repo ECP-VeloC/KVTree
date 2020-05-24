4395ad1be7d7aa8959670fea70eb147c59d2f4cc
Adam Moody <moody20@llnl.gov>
2020-05-24 12:51:17 -0700

    kvtree_print: add --mode option to select print format

--- src/kvtree_print.c
+++ src/kvtree_print.c
@@ -2,7 +2,6 @@
 
 #include "kvtree.h"
 #include "kvtree_io.h"
-#include "kvtree_err.h"
 #include "kvtree_helpers.h"
 #include "kvtree.h"
 
@@ -13,45 +12,96 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <getopt.h>
+
+static void print_usage(void)
+{
+  printf("\n");
+  printf("Usage: kvtree_print [options] <file>\n");
+  printf("\n");
+  printf("  Options:\n");
+  printf("    -m, --mode <mode>  Specify print format: \"tree\" or \"keyval\" (default tree)\n");
+  printf("    -h, --help         Print usage\n");
+  printf("\n");
+}
 
 int main(int argc, char* argv[])
 {
   int rc = 0;
 
-  /* check that we were given exactly one filename argument */
-  if (argc != 2) {
-    printf("Usage: kvtree_print_hash_file <hashfile>\n");
-    return 1;
-  }
+  static const char *opt_string = "m:h";
+  static struct option long_options[] = {
+    {"mode",    required_argument, NULL, 'm'},
+    {"help",    no_argument,       NULL, 'h'},
+    {NULL,      no_argument,       NULL,   0}
+  };
 
-  /* read in the filename */
-  char* filename = strdup(argv[1]);
-  if (filename != NULL) {
-    /* get a hash to read in the file */
-    kvtree* hash = kvtree_new();
+  int usage = 0;
+  char* mode = NULL;
 
-    /* read in the file */
-    if (kvtree_read_file(filename, hash) == KVTREE_SUCCESS) {
-      /* we read the file, now print it out */
-      kvtree_print(hash, 0);
-    } else {
-      kvtree_err("Could not read file %s @ %s:%d",
-              filename, __FILE__, __LINE__
-      );
-      rc = 1;
+  int long_index = 0;
+  while (1) {
+    char c = getopt_long(argc, argv, opt_string, long_options, &long_index);
+    if (c == -1) {
+      break;
     }
 
-    /* free the hash object */
-    kvtree_delete(&hash);
+    switch(c) {
+      case 'm':
+        mode = strdup(optarg);
+        break;
+      case 'h':
+        usage = 1;
+        break;
+      default:
+        printf("ERROR: Unknown option: `%s'\n", argv[optind]);
+        usage = 1;
+        rc = 1;
+        break;
+    }
+  }
 
-    /* free our strdup'd filename */
-    kvtree_free(&filename);
-  } else {
-    kvtree_err("Could not copy filename from command line @ %s:%d",
-            __FILE__, __LINE__
-    );
+  /* check that we were given exactly one filename argument */
+  int numargs = argc - optind;
+  if (!usage && numargs != 1) {
+    printf("ERROR: Missing file name or too many files\n");
+    usage = 1;
     rc = 1;
   }
+
+  /* parse the print mode option, if one is given */
+  int print_mode = KVTREE_PRINT_TREE;
+  if (mode != NULL) {
+    if (strcmp(mode, "tree") == 0) {
+      print_mode = KVTREE_PRINT_TREE;
+    } else if (strcmp(mode, "keyval") == 0) {
+      print_mode = KVTREE_PRINT_KEYVAL;
+    } else {
+      printf("ERROR: Invalid mode name: `%s'\n", mode);
+      usage = 1;
+      rc = 1;
+    }
+    free(mode);
+  }
+
+  if (usage) {
+    print_usage();
+    return rc;
+  }
+
+  /* get the file name */
+  char* filename = argv[optind];
+
+  /* read in the file */
+  kvtree* hash = kvtree_new();
+  if (kvtree_read_file(filename, hash) == KVTREE_SUCCESS) {
+    /* we read the file, now print it out */
+    kvtree_print_mode(hash, 0, print_mode);
+  } else {
+    printf("ERROR: Failed to read file: `%s'\n", filename);
+    rc = 1;
+  }
+  kvtree_delete(&hash);
 
   return rc;
 }
