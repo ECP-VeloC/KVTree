1f30177da99ad2299acfabca3573dfd551f6d2fa
Gregory A. Kosinovsky <kosinov@quartz1922.llnl.gov>
2020-08-12 16:46:09 -0700

    fix typos

--- cmake/KVTREE_ADD_TEST.cmake
+++ cmake/KVTREE_ADD_TEST.cmake
@@ -1,13 +1,13 @@
 function(KVTREE_ADD_TEST name args outputs)
 
   # job launcher
-  if(${SCR_RESOURCE_MANAGER} STREQUAL "NONE")
+  if(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
     set(test_param mpirun -np 1)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
     set(test_param jsrun -r 1)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "SLURM")
     set(test_param srun -N 3 -n 3)
-  endif(${SCR_RESOURCE_MANAGER} STREQUAL "NONE")
+  endif(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
 
   # Tests
   add_test(NAME ${name} COMMAND ${test_param} ./${name} ${args} )
