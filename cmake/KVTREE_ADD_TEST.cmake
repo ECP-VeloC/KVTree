e2e719980bfbebf1d14182d2e6298f66a3030866
Gregory A. Kosinovsky <kosinov@quartz2306.llnl.gov>
2020-08-19 15:33:58 -0700

    removed requirement to define VELOC_RESOURCE_MANAGER variable on the cmake line

--- cmake/KVTREE_ADD_TEST.cmake
+++ cmake/KVTREE_ADD_TEST.cmake
@@ -1,14 +1,15 @@
 function(KVTREE_ADD_TEST name args outputs)
 
   # job launcher
-  if(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
+  if(NOT(${VELOC_RESOURCE_MANAGER}))
+    set(test_param mpirun -np 1)
+  elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
     set(test_param mpirun -np 1)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
     set(test_param jsrun -r 1)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "SLURM")
     set(test_param srun -N 3 -n 3)
-  endif(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
-#  endif()
+  endif()
  
   # Tests
   add_test(NAME ${name} COMMAND ${test_param} ./${name} ${args} )
