01d072972299c25a6fba46b1599ef3b465105a54
Gregory A. Kosinovsky <kosinov@quartz2306.llnl.gov>
2020-08-24 12:49:53 -0700

    fixed cmake function

--- cmake/KVTREE_ADD_TEST.cmake
+++ cmake/KVTREE_ADD_TEST.cmake
@@ -1,10 +1,12 @@
 function(KVTREE_ADD_TEST name args outputs)
 
   # job launcher
-  if(NOT(${VELOC_RESOURCE_MANAGER}))
-    set(test_param mpirun -np 1)
+  if(NOT DEFINED VELOC_RESOURCE_MANAGER)
+#    set(test_param mpirun -np 3)
+#    set(test_param jsrun -r 1)
+    set(test_param srun -N 3 -n 3)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
-    set(test_param mpirun -np 1)
+    set(test_param mpirun -np 3)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
     set(test_param jsrun -r 1)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "SLURM")
